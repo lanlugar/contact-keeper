36d4f4cdf3047c4a911050d9d0460cc687e06db9
Lugar Lan <lanlugar@gmail.com>
2020-06-17 05:51:04 +0100

    setting up routes

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 5000;
 
 // Define Routes
 
-app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);

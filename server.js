5da15dd9c2e5026b6f7413d67ce2b62b85cc5f09
Lugar Lan <lanlugar@gmail.com>
2020-06-19 14:20:04 +0100

    JWT implementation

--- server.js
+++ server.js
@@ -7,13 +7,16 @@ const app = express();
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 // Define Routes
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
